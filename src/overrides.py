11967b45e89ac48e85db21c22708382c5e7cdb8a
Lincoln de Sousa <lincoln@comum.org>
2010-05-03 17:46:43 -0300

    First not actually good wrap to send_and_filter method
    The ta_xmpp_client_send_end_filter() method is quite complex
    to wrap since it receives a callback to be fired when the
    message arrives. This implementation uses the same stuff used
    by event_connect().

--- src/overrides.py
+++ src/overrides.py
@@ -97,6 +97,90 @@ _wrap_ta_xmpp_client_event_connect(PyTa_xmpp_client_t *self,
 }
 '''
 
+ta_xmpp_client_send_and_filter = '''
+static void
+_call_py_xmpp_client_send_and_filter (ta_xmpp_client_t *PYBINDGEN_UNUSED(client),
+                                      iks *stanza,
+                                      void *data)
+{
+    PyObject *result, *args;
+    PyIks *pystanza;
+    struct py_hook_data *hdata = (struct py_hook_data *) data;
+    PyGILState_STATE threadstate;
+
+    threadstate = PyGILState_Ensure ();
+    pystanza = PyObject_New(PyIks, &PyIks_Type);
+    pystanza->obj = stanza;
+    pystanza->flags = PYBINDGEN_WRAPPER_FLAG_OBJECT_NOT_OWNED;
+    args = Py_BuildValue ("(OO)", hdata->data, stanza);
+    result = PyObject_CallObject (hdata->callback, args);
+
+    /* Time to free stuff */
+    Py_DECREF (args);
+    if (result == NULL)
+        PyErr_Print ();
+    else
+        Py_DECREF (result);
+    free (hdata);
+
+    PyGILState_Release (threadstate);
+}
+
+static PyObject *
+_wrap_ta_xmpp_client_send_and_filter(PyTa_xmpp_client_t *self,
+                                     PyObject *args,
+                                     PyObject *kwargs,
+                                     PyObject **return_exception)
+
+{
+    PyObject *py_retval;
+    PyObject *stanza;
+    int retval;
+    const char *keywords[] = {"stanza", "callback", "user_data", NULL};
+    PyObject *callback = NULL;
+    PyObject *user_data = NULL;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "OO|O", (char **)keywords,
+                                     &stanza, &callback, &user_data)) {
+        PyObject *exc_type, *traceback;
+        PyErr_Fetch(&exc_type, return_exception, &traceback);
+        Py_XDECREF(exc_type);
+        Py_XDECREF(traceback);
+        return NULL;
+    }
+    if (0) {
+        PyErr_SetString (PyExc_TypeError, "Param 1 must be an Iks instance");
+        return NULL;
+    }
+    if (!PyCallable_Check (callback)) {
+        PyErr_SetString (PyExc_TypeError, "Param 2 must be callable.");
+        return NULL;
+    } else {
+        PyObject *data = NULL;
+        struct py_hook_data *hook_data;
+        if (user_data)
+            data = user_data;
+        else
+            data = Py_None;
+
+        hook_data = malloc (sizeof (struct py_hook_data));
+
+        Py_INCREF (callback);
+        hook_data->callback = callback;
+
+        Py_INCREF (data);
+        hook_data->data = data;
+        retval = ta_xmpp_client_send_and_filter (self->obj,
+                     ((PyIks *) stanza)->obj,
+                     _call_py_xmpp_client_send_and_filter,
+                     hook_data, NULL);
+
+        py_retval = Py_BuildValue((char *) "i", retval);
+        return py_retval;
+    }
+}'''
+
 OVERRIDES = {
     'ta_xmpp_client_event_connect': ta_xmpp_client_event_connect,
+    'ta_xmpp_client_send_and_filter': ta_xmpp_client_send_and_filter,
 }
